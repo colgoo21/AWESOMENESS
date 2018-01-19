255b56959e8f7c9d8714175d19372d7c0fddad12
Cole <colgoo21@bergen.org>
2018-01-19 13:11:00 -0500

    KILL YOURSELF

--- Cole_Demo3.py
+++ Cole_Demo3.py
@@ -8,11 +8,12 @@ pygame.display.set_caption("SPACE INVADERS BETA")
 
 black = (0, 0, 0)
 white = (255, 255, 255)
-
+red = (255, 0, 0)
 
 moveX = 0
 
 clock = pygame.time.Clock()
+
 
 class Sprite:
     def __init__(self, x, y):
@@ -25,67 +26,55 @@ class Sprite:
     def render(self):
         window.blit(self.image, (self.x, self.y))
 
-    def shoot_bullet(self):
-        bullet = pygame.image.load('bullet.png')
-        self.width = 5
-        self.height = 30
-        bullet.x = self.x
-        bullet.y = 0
-        while bullet.y != 550:
-            bullet += 1
-        if bullet.y == 570:
-            del(bullet)
-
-    def dont_shoot_bullet(self):
-        del(self.shoot_bullet)
-
-class Enemy:
-    def __init__(self, x, y):
-        # 8 by 5
-        self.x = x
-        self.y = y
-        self.width = 50
-        self.height = 17
-        self.image = pygame.image.load('enemy.png')
-
-    def eight_by_five(self):
-        enemy = 0
-        while enemy != 40:
-            enemy.x = 0
-            enemy.y = 0
-            enemy.x += 10
-            enemy += 1
-            if enemy % 8 == 0:
-                enemy.x = 0
-                enemy.y = 20
-
+    def shoot(self):
+        laser = Laser(self.)
 
 
 player = Sprite(350, 450)
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(red)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+    def render(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
 
 gameloop = True
 while gameloop:
     for event in pygame.event.get():
         if player.x <= 0:
             player.x = 0
-        if player.x >= 750:
-            player.x = 750
+
+        if player.x >= 550:
+            player.x = 550
+
         if event.type == pygame.QUIT:
             gameloop = False
+
         if event.type == pygame.KEYDOWN:
+
             if event.key == pygame.K_LEFT:
                 moveX = -5
+
             if event.key == pygame.K_RIGHT:
                 moveX = 5
-            if event.key == pygame.K_SPACE:
-                player.shoot_bullet()
+
         if event.type == pygame.KEYUP:
+
             if event.key == pygame.K_LEFT:
                 moveX = 0
+
             if event.key == pygame.K_RIGHT:
                 moveX = 0
-            if event.key == pygame.K_SPACE:
-                player.dont_shoot_bullet()
 
     window.fill(black)
     player.x += moveX
