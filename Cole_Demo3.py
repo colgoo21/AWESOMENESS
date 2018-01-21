75d21c6fa75cbe6837927a6f497dba9d988300fc
Cole <colgoo21@bergen.org>
2018-01-21 15:05:57 -0500

    KILL YOURSELF

--- Cole_Demo3.py
+++ Cole_Demo3.py
@@ -1,83 +1,83 @@
 import pygame
 
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 pygame.init()
-
-window = pygame.display.set_mode((800, 600))
-
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SPACE INVADERS BETA")
-
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (255, 0, 0)
-
-moveX = 0
-
 clock = pygame.time.Clock()
 
-class Sprite:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.width = 50
-        self.height = 50
-        self.image = pygame.image.load('alien.png')
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load('alien.png'), (50, 54))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
 
-    def render(self):
-        window.blit(self.image, (self.x, self.y))
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 10
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -10
+        self.rect.x += self.speedx
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
 
     def shoot(self):
-        laser = Laser(self.)
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
-
-player = Sprite(350, 450)
-
-
-class Laser(pygame.sprite.Sprite):
+class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(red)
+        self.image = pygame.Surface((5, 30))
+        self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
         self.speedy = -10
-    def render(self):
+
+    def update(self):
         self.rect.y += self.speedy
         if self.rect.bottom < 0:
             self.kill()
 
+all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
-gameloop = True
-while gameloop:
+
+running = True
+while running:
+    clock.tick(FPS)
     for event in pygame.event.get():
-        if player.x <= 0:
-            player.x = 0
-
-        if player.x >= 550:
-            player.x = 550
-
         if event.type == pygame.QUIT:
-            gameloop = False
-
+            running = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
-            if event.key == pygame.K_LEFT:
-                moveX = -5
+    all_sprites.update()
 
-            if event.key == pygame.K_RIGHT:
-                moveX = 5
-
-        if event.type == pygame.KEYUP:
-
-            if event.key == pygame.K_LEFT:
-                moveX = 0
-
-            if event.key == pygame.K_RIGHT:
-                moveX = 0
-
-    window.fill(black)
-    player.x += moveX
-    player.render()
-    clock.tick(50)
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
     pygame.display.flip()
+
 pygame.quit()
